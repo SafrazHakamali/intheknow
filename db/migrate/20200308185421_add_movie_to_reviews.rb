8f13a5b4b6ff767bc8252eb0ee46482509f1e82e
SafrazHakamali <safrazhakamali@gmail.com>
2020-03-11 15:33:25 +0800

    correcting db post merge

--- db/migrate/20200308185421_add_movie_to_reviews.rb
+++
@@ -1,5 +0,0 @@
-class AddMovieToReviews < ActiveRecord::Migration[6.0]
-  def change
-    add_column :reviews, :movie_id, :integer
-  end
-end
--- db/migrate/20200310070556_create_reviews.rb
+++ db/migrate/20200310070556_create_reviews.rb
@@ -5,7 +5,6 @@ class CreateReviews < ActiveRecord::Migration[6.0]
       t.text :comment
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :movie, null: false, foreign_key: true
-
       t.timestamps
     end
   end
